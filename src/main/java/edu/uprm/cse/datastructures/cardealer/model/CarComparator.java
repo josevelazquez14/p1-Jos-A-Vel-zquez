06004f427366928e772f13753b21934c4c5f934c
José A. Velázquez Torres <jose.velazquez14@upr.edu>
2018-10-01 18:17:36 -0400

    carComparator and CircularSortedDoublyLinkedList

---
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarComparator.java
@@ -0,0 +1,19 @@
+package edu.uprm.cse.datastructures.cardealer.model;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+
+
+public class CarComparator<T> implements Comparator<T>{
+
+
+	@Override
+	public int compare(T o1, T o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+}
